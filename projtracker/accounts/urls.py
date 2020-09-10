636a7d792a8b6cc85c19ab54a6595fd7bef276bf
arun <arunsai4b6@gmail.com>
2020-09-10 18:46:01 +0530

    redirect error:fixed

--- projtracker/accounts/urls.py
+++ projtracker/accounts/urls.py
@@ -3,9 +3,9 @@ from . import views
 from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
-    path('', views.indexView, name ="home"),
+    path('home/', views.indexView, name ="home"),
     path('dashboard/',views.dashboardView, name="dashboard"),
-    path('login/',LoginView.as_view(), name= "login_url"),
+    path('',LoginView.as_view(), name= "login_url"),
     path('register/', views.registerView, name= "register_url"),
-    path('logout/',LogoutView.as_view(next_page='dashboard'), name= "logout"),
+    path('logout/',LogoutView.as_view(next_page='login_url'), name= "logout"),
 ]
--- projtracker/projtracker/settings.py
+++ projtracker/projtracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'enhs%k%c^oc+rh+%(c1aw-7=_qr+_jklc1m7trb8882x-44sm='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['4c5ab8b080ae.ngrok.io','127.0.0.1:8000']
 
 
 # Application definition
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/accounts/dashboard/'
+LOGIN_REDIRECT_URL = '/dashboard/'
